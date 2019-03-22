ccbed445cea914bee4381c4b4b572e4595df88e3
ayusartikasari <ayusartikasari021@gmail.com>
2019-03-21 17:05:16 -1200

    tokap21

--- app/Http/Controllers/kategoriController.php
+++ app/Http/Controllers/kategoriController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Kategori;
 
 class kategoriController extends Controller
 {
     public function daftar(Request $req)
     {
-    	return view('admin.pages.kategori.daftar');
+    	$data = kategori::where('nama_kategori','like',"%{$req->keyword}%")
+    			->paginate(10);
+
+    	return view('admin.pages.kategori.daftar',['data'=>$data]);
     }
 }
